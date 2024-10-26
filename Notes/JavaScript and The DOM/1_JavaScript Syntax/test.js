a0a5015eb15b1d0e0a0639ba4cb230f2b94dfdf3
MrIzzat <118838678+MrIzzat@users.noreply.github.com>
2024-10-26 20:57:32 +0200

    Removed temp files

--- Notes/JavaScript and The DOM/1_JavaScript Syntax/test.js
+++
@@ -1,7 +0,0 @@
-const upperizedNames = ['Farrin', 'Kagure', 'Asser'].map(
-    name => {
-        name.toLowerCase();
-        name.toUpperCase();
-    return name;});
-
-console.log(...upperizedNames)
