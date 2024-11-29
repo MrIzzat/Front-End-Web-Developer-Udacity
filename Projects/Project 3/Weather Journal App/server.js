a4db969813a8a96404476625b69556b51232b884
MrIzzat <118838678+MrIzzat@users.noreply.github.com>
2024-11-29 22:34:42 +0200

    Added The Skeleton Files Of Project 3

---
+++ Projects/Project 3/Weather Journal App/server.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+projectData = {};
+
+const port = 8000;
+
+
+app.use(express.static(path.join(__dirname,"/website")));
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+app.use(cors());
+
+
+const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+app.get("/projectData",(req, res) => {
+    res.send(JSON.stringify(projectData));
+})
