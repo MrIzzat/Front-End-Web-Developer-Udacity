a1c081e92d4b015cfc25d43c90390c08905eca8a
MrIzzat <118838678+MrIzzat@users.noreply.github.com>
2024-11-30 12:19:23 +0200

    Created Function For Client to GET Project Data from Server

--- Projects/Project 3/Weather Journal App/server.js
+++ Projects/Project 3/Weather Journal App/server.js
@@ -21,6 +21,8 @@ app.use(cors());
 const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
+projectData["TEST"] = "TEST";
+
 app.get("/projectData",(req, res) => {
     res.send(JSON.stringify(projectData));
 })
--- Projects/Project 3/Weather Journal App/website/script.js
+++ Projects/Project 3/Weather Journal App/website/script.js
@@ -1,4 +1,18 @@
-console.log("TEST");
+let projectData = {};
+
+const getProjectData = async (url="") => {
+    const response = await fetch(url);
+
+    try{
+        const data = await response.json();
+        return data;
+    }
+    catch (error){
+        console.log(`Error: ${error}`);
+    }
+}
 
 
-function getProjectData() {}
+
+getProjectData("/projectData").then((data) => {projectData=data; console.log(projectData);});
+
