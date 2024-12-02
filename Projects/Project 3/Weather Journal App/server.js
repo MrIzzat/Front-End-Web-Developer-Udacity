5d9dd3cbbd8858cbc797ac9842de1cf93d55150b
MrIzzat <118838678+MrIzzat@users.noreply.github.com>
2024-12-02 21:08:07 +0200

    Added Retrieving Data from OpenWeatherMap API and sending to the server

--- Projects/Project 3/Weather Journal App/server.js
+++ Projects/Project 3/Weather Journal App/server.js
@@ -21,8 +21,6 @@ app.use(cors());
 const server = app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
-projectData["TEST"] = "TEST";
-
 app.get("/projectData",(req, res) => {
     res.send(JSON.stringify(projectData));
 })
--- Projects/Project 3/Weather Journal App/website/script.js
+++ Projects/Project 3/Weather Journal App/website/script.js
@@ -1,3 +1,9 @@
+const apiKey = "cef657663967c9e4808ab1bb77fe3cd4";
+const baseURL = "https://api.openweathermap.org/data/2.5/forecast?";
+
+
+//const urlT = `https://api.openweathermap.org/data/2.5/forecast?lat=32.204486&lon=35.281958&cnt=1&APPID=cef657663967c9e4808ab1bb77fe3cd4`;
+
 let projectData = {};
 
 const getProjectData = async (url="") => {
@@ -14,17 +20,17 @@ const getProjectData = async (url="") => {
 
 
 
-getProjectData("/projectData").then((data) => {projectData=data; console.log(projectData);});
+//getProjectData("/projectData").then((data) => {projectData=data; console.log(projectData);});
 
 
-const postProjectData = async (url="") => {
+const postProjectData = async (url="", data) => {
     const response = await fetch(url,{
         method : "POST",
         credentials : "same-origin",
         headers: {
             "Content-Type" : "application/json"
         },
-        body : JSON.stringify({temperature : "temperature", date : "date", userResponse : "userResponse"})
+        body : JSON.stringify(data)
     });
 
     try{
@@ -36,4 +42,45 @@ const postProjectData = async (url="") => {
     }
 }
 
-postProjectData("/addToProjectData").then((data) => {projectData=data; console.log(projectData);});
+//postProjectData("/addToProjectData").then((data) => {projectData=data; console.log(projectData);});
+
+
+
+
+
+
+const getWeatherInformation = async (url="") => {
+    const response = await fetch(url);
+
+
+    try{
+        const data = await response.json();
+        // console.log("hell yeah");
+        // console.log(data.list[0].dt_txt);//Date as a string
+        // console.log(data.list[0].main.temp)// temperature in kelvin
+
+        return data;
+    }
+    catch (error){
+        console.log(`Error: ${error}`);
+    }
+}
+
+
+
+//getWeatherInformation(weatherURL).then((data) => {projectData=data; console.log(projectData);});
+
+const btnGenerate = document.getElementById("generate");
+const txtZip = document.getElementById("zip");
+const txtResponse = document.getElementById("feelings");
+
+btnGenerate.addEventListener("click",(event) => {
+    let response = txtResponse.value;
+    let zipCode = txtZip.value;
+
+    const weatherURL = `${baseURL}zip=${zipCode}&cnt=${1}&APPID=${apiKey}`;
+
+    getWeatherInformation(weatherURL)
+    .then((data) => postProjectData("/addToProjectData",
+        {date : data.list[0].dt_txt, temperature : data.list[0].main.temp,userResponse : response}));
+});
